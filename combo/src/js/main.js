111b14d95673bd701a92632deee353a77eb37a27
PHOENIXIT\ekanarskaya <ekanarskaya@fil-it.ru>
2023-07-29 18:27:36 +0300

    add markup

--- combo/src/js/main.js
+++ combo/src/js/main.js
@@ -1,17 +1,11 @@
-import $ from 'jquery';
 import { WOW } from './vendor/wow.min';
 import detectDevice from './components/detectDevice';
 
-import { closeModal, openModal } from './components/modal';
-import {
-  fieldListener, validateFields, keyField, prepField,
-} from './components/inputs';
-import generateId from './components/utils';
+import { openModal } from './components/modal';
 import GTMEvents from './components/gtmEvents';
 
 const GTM = new GTMEvents();
 
-window.jQuery = window.$ = $;
 /// /////// DocReady //////////
 window.addEventListener('load', () => {
   detectDevice(); // videoTeaser();
@@ -19,127 +13,15 @@ window.addEventListener('load', () => {
 
   GTM.addEventListeners();
   goNextSection();
-  const $body = document.querySelector('body');
+  openPopup();
+  handleFAQopening();
+});
 
-  // открыть модалку
-  $body.addEventListener('click', (e) => {
-    const _cl = 'js-tryit';
-    const $showPop = (e.target.classList.contains(_cl)) ? e.target : e.target.closest(`.${_cl}`);
-    if ($showPop) { openModal('#req-modal-box'); }
-    // let pops = document.querySelectorAll('.modal');[...pops].forEach(pop => {});
-  });
-
-  // Показать поле ИНН
-  const innF = document.querySelectorAll('.js-toggle-inn'); [...innF].forEach((_btn) => {
-    _btn.addEventListener('click', (bt) => {
-      const _holder = document.querySelector('.js-inn-holder');
-      const _v = '_visible'; const
-        _innFld = document.querySelector('.js-inn');
-      if (!bt.target.classList.contains('on')) { _holder.classList.add(_v); _innFld.focus(); } else {
-        _holder.classList.remove(_v); _innFld.value = '';
-        // focus on button form
-      }
-      bt.target.classList.toggle('on');
-    });
-  });
-
-  // INPUTS
-  fieldListener($body, ['mousemove', 'mouseover', 'mouseout', 'blur'], prepField);
-  fieldListener($body, ['focus', 'click', 'beforeinput', 'keydown', 'change', 'paste'], keyField);
-
-  // BUTTON SUBM
-  const $formBtn = document.querySelectorAll('.js-new-req');
-  [...$formBtn].forEach((btn) => { btn.addEventListener('click', (e) => { e.preventDefault(); trySubmit(); }); });
-
-  function trySubmit() {
-    validateFields();
-    document.querySelector('.selectorinn').style.display = 'none';
-    const _erCount = document.querySelectorAll('.text-error').length;
-    const frmReady = (_erCount === 0);
-    if (frmReady) {
-      submitCustomFormRequest();
-    } else {
-      console.warn('UNREADY SUBMIT');
-    }
-  }
-
-  /*  let $fish = document.querySelectorAll('.fish');
-  [... $fish].forEach( (ff) => {ff.addEventListener('click',(e)=> {e.preventDefault();
-    ff.classList.toggle('ss');
-  });}); */
-}); // end DocReady
-/// ///////////////////////////
-
-function toggleLoader() {
-  const loader = document.querySelector('.loader');
-  loader.classList.toggle('hidden');
-}
-
-function submitCustomFormRequest() {
-  const $frm = document.querySelector('.crm-request-form');
-  const $submitBtn = document.querySelector('.js-new-req');
-  const innField = document.querySelector('#innField');
-
-  const inn = (innField.value.length >= 10) ? innField.value : '';
-  // document.querySelector('#innField').value || '';
-  const name = document.querySelector('#nameField').value;
-  const phone = document.querySelector('#telField').value;
-
-  const siteId = localStorage.getItem('siteId') || 'siteMSK';
-  const url = `api/customForm/submission?siteId=${siteId}&formId=CrmMyTeamRequest`;
-  // const tariffFrontName = 'corp-ats';
-
-  const requestBody = {
-    MainContact: name,
-    Phone: phone,
-    requestCategory: '1',
-    requestId: `${Date.now()}_${Math.random().toString().slice(2, 12)}`,
-    region: siteId.slice(4).toLowerCase(),
-    InnCompany: inn,
-    AdditionalInformation: 'Подключение услуги corp-ats',
-    // tariffs: [{ name: tariffFrontName, }],
-  };
-
-  toggleLoader();
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({ data: requestBody }),
-    headers: { Accept: 'application/json, text/plain', 'Content-Type': 'application/json;charset=UTF-8' },
-  })
-    .then(() => { // console.log(r);
-      const reqID = requestBody.requestId;
-      const dataSuccess = {
-        eventAction: 'send',
-        eventLabel: 'checkout_corp-ats-form_server_response',
-        requestId: reqID,
-        eventCategory: 'Conversions',
-        eventContent: `corp-ats_${Date.now()}${generateId(12)}`,
-        eventContext: 'successful',
-        transactionPaymentType: null,
-        transactionShippingMethod: null,
-      };
-      GTM.gaPush(dataSuccess); // console.log(dataSuccess);
-      closeModal('#req-modal-box');
-      openModal('#succ-modal-box');
-    })
-    .catch(() => {
-      closeModal('#req-modal-box');
-      openModal('#error-modal-box');
-      console.log('something wrong');
-      const reqID = requestBody.requestId;
-      const dataSuccess = {
-        eventAction: 'send',
-        eventLabel: 'checkout_corp-ats-form_server_response',
-        requestId: reqID,
-        eventCategory: 'Conversions',
-        eventContent: `shop_corp-ats_${Date.now()}${generateId(12)}`,
-        eventContext: 'unsuccessful',
-        transactionPaymentType: null,
-        transactionShippingMethod: null,
-      };
-      GTM.gaPush(dataSuccess);
-    })
-    .finally(() => toggleLoader());
+// scroll to next section
+function scrollToElement(el) {
+  const offs = 0;
+  const y = el.getBoundingClientRect().top + window.scrollY + offs;
+  window.scrollTo({ top: y, behavior: 'smooth' }); // element.scrollIntoView();
 }
 
 function goNextSection() {
@@ -159,9 +41,38 @@ function goNextSection() {
     });
   });
 }
+// open pop-up modal
+function openPopup() {
+  const popupLink = document.querySelector('.popup__link');
+  popupLink.addEventListener('click', () => openModal('#popup-modal-box'));
+}
 
-function scrollToElement(el) {
-  const offs = 0;
-  const y = el.getBoundingClientRect().top + window.scrollY + offs;
-  window.scrollTo({ top: y, behavior: 'smooth' }); // element.scrollIntoView();
+function setActive(arr) {
+  const activeClassName = 'active';
+  arr.forEach((el) => {
+    const itemText = el.childNodes[3]; // хардкод текстового дочернего узла
+    if (el.classList.contains(activeClassName)) {
+      itemText.style.transition = 'none';
+      el.classList.remove(activeClassName);
+    }
+  });
+}
+
+// open FAQ-items
+function handleFAQopening() {
+  const itemsList = document.querySelectorAll('.faq__item');
+  const activeClassName = 'active';
+  itemsList.forEach((item) => {
+    item.addEventListener('click', () => {
+      const itemText = item.childNodes[3]; // хардкод текстового дочернего узла
+      if (item.classList.contains(activeClassName)) {
+        itemText.style.transition = 'none';
+        item.classList.remove(activeClassName);
+      } else {
+        setActive(itemsList);
+        itemText.style.transition = '0.2s ease-in-out';
+        item.classList.add(activeClassName);
+      }
+    });
+  });
 }
